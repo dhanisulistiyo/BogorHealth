85f4fc3d59b8cb03922b96d1d06768308f0c8277
DHANI SULISTIYO WIBOWO <dhani.s.wibowo@gmail.com>
2018-01-04 21:21:07 +0700

    add class person and pasien

---
+++ Application/BoSe/src/model/pasien.ts
@@ -0,0 +1,15 @@
+import { Person } from "./person";
+
+export class Pasien extends Person {
+    NIK 
+    Pekerjaan
+    StatusPernikahan
+    Password
+    constructor() {
+        super();
+        this.NIK = null;
+        this.Pekerjaan = null
+        this.StatusPernikahan = null
+        this.Password = null
+    }
+}
---
+++ Application/BoSe/src/model/person.ts
@@ -0,0 +1,20 @@
+export class Person {
+    Nama
+    Alamat
+    TanggalLahir
+    KotaLahir
+    Email
+    JenisKelamin
+    Agama
+    ImageUrl
+    constructor() {
+        this.Nama = null
+        this.Alamat = null
+        this.TanggalLahir = null
+        this.KotaLahir = null
+        this.Email = null
+        this.JenisKelamin = null
+        this.Agama = null
+        this.ImageUrl = null
+    }
+}
