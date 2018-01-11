c1e1737cf9386e9eacb57c01991a433c4f8aac64
DHANI SULISTIYO WIBOWO <dhani.s.wibowo@gmail.com>
2018-01-11 10:53:14 +0700

    bug fix list-hospital

--- Application/BoSe/src/pages/list-hospital/list-hospital.ts
+++ Application/BoSe/src/pages/list-hospital/list-hospital.ts
@@ -17,7 +17,9 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ListHospitalPage {
   Hospital
+  baseUrl
   constructor(public navCtrl: NavController, public navParams: NavParams, public hos: HospitalServiceProvider) {
+    this.baseUrl="http://localhost:53939/Content/img/"
   }
 
   ionViewWillEnter(){
